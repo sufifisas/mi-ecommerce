325510314ec7c0bf64157fee8b5beda8f6718a5e
sufifisas <sufifisas@gmail.com>
2023-09-22 11:19:47 +0800

    Dev: added dummy product card list

--- client/src/pages/product/index.jsx
+++ client/src/pages/product/index.jsx
@@ -1,13 +1,20 @@
 import { Link } from "react-router-dom";
 import Search from "../../components/Search";
 import MainLayout from "../../layouts/MainLayout";
+import ProductCard from "../../components/ProductCard";
 
 const Product = () => {
   return (
-    <MainLayout>
-        <div>Product</div>
+    <MainLayout title="Products">
         <Search />
-        <Link to="/1">Go to Product 1</Link>
+        <div className="grid grid-cols-4 mt-8 gap-x-4 gap-y-6">
+          <ProductCard brand="Vegetables" title="Kicap Lemak Manis" code="123456789012" url="2323" />
+          <ProductCard brand="adabi" title="Kicap Lemak Manis Kicap Lemak Manis" code="123456789012" url="2323" />
+          <ProductCard brand="adabi" title="Kicap Lemak Manis" code="123456789012" url="2323" />
+          <ProductCard brand="adabi" title="Kicap Lemak Manis" code="123456789012" url="2323" />
+          <ProductCard brand="adabi" title="Kicap Lemak Manis" code="123456789012" url="2323" />
+        </div>
+        {/* <Link to="/1">Go to Product 1</Link> */}
     </MainLayout>
   )
 }
